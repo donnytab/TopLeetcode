b6475d1df1805b19ef8a62cae5700266da90d556
downson <downson.yuen@gmail.com>
2023-11-09 23:58:00 +0800

    LC345 LC347

---
+++ sort/LC345_topK-frequent.cpp
@@ -0,0 +1,82 @@
+class Solution {
+public:
+    static bool cmp(pair<int, int>& m, pair<int, int>& n) {
+        return m.second > n.second;
+    }
+
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> occurrences;
+        for (auto& v : nums) {
+            occurrences[v]++;
+        }
+
+        // pair 的第一个元素代表数组的值，第二个元素代表了该值出现的次数
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(&cmp)> q(cmp);
+        for (auto& [num, count] : occurrences) {
+            if (q.size() == k) {
+                if (q.top().second < count) {
+                    q.pop();
+                    q.emplace(num, count);
+                }
+            } else {
+                q.emplace(num, count);
+            }
+        }
+        vector<int> ret;
+        while (!q.empty()) {
+            ret.emplace_back(q.top().first);
+            q.pop();
+        }
+        return ret;
+    }
+};
+
+class Solution {
+public:
+    void qsort(vector<pair<int, int>>& v, int start, int end, vector<int>& ret, int k) {
+        int picked = rand() % (end - start + 1) + start;
+        swap(v[picked], v[start]);
+
+        int pivot = v[start].second;
+        int index = start;
+        for (int i = start + 1; i <= end; i++) {
+            // 使用双指针把不小于基准值的元素放到左边，
+            // 小于基准值的元素放到右边
+            if (v[i].second >= pivot) {
+                swap(v[index + 1], v[i]);
+                index++;
+            }
+        }
+        swap(v[start], v[index]);
+
+        if (k <= index - start) {
+            // 前 k 大的值在左侧的子数组里
+            qsort(v, start, index - 1, ret, k);
+        } else {
+            // 前 k 大的值等于左侧的子数组全部元素
+            // 加上右侧子数组中前 k - (index - start + 1) 大的值
+            for (int i = start; i <= index; i++) {
+                ret.push_back(v[i].first);
+            }
+            if (k > index - start + 1) {
+                qsort(v, index + 1, end, ret, k - (index - start + 1));
+            }
+        }
+    }
+
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        // 获取每个数字出现次数
+        unordered_map<int, int> occurrences;
+        for (auto& v: nums) {
+            occurrences[v]++;
+        }
+
+        vector<pair<int, int>> values;
+        for (auto& kv: occurrences) {
+            values.push_back(kv);
+        }
+        vector<int> ret;
+        qsort(values, 0, values.size() - 1, ret, k);
+        return ret;
+    }
+};
